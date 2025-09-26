202c9f264fe478e22fc85facf923aa7a3ddf43f9
Michael438-sudo <michael.robinson26@compscihigh.org>
2025-09-26 00:49:25 +0000

    complete code

--- index.js
+++ index.js
@@ -3,22 +3,22 @@
 // Let's first identify what parts of the code is repetitive
 
 // Superhero 1
-let hero1 = "Spider-Man";
-let power1 = "web-slinging";
-let city1 = "New York City";
-console.log(hero1 + " uses " + power1 + " to protect " + city1 + ".");
+// let hero1 = "Spider-Man";
+// let power1 = "web-slinging";
+// let city1 = "New York City";
+// console.log(hero1 + " uses " + power1 + " to protect " + city1 + ".");
 
-// Superhero 2
-let hero2 = "Magneto";
-let power2 = "magnetic field manipulation";
-let city2 = "Genosha";
-console.log(hero2 + " uses " + power2 + " to protect " + city2 + ".");
+// // Superhero 2
+// let hero2 = "Magneto";
+// let power2 = "magnetic field manipulation";
+// let city2 = "Genosha";
+// console.log(hero2 + " uses " + power2 + " to protect " + city2 + ".");
 
-// Superhero 3
-let hero3 = "Wonder Woman";
-let power3 = "super strength";
-let city3 = "Themyscira";
-console.log(hero3 + " uses " + power3 + " to protect " + city3 + ".");
+// // Superhero 3
+// let hero3 = "Wonder Woman";
+// let power3 = "super strength";
+// let city3 = "Themyscira";
+// console.log(hero3 + " uses " + power3 + " to protect " + city3 + ".");
 
 // Now let's work together to make this code more modular. 
 // We'll start by defining a function that can handle the repetitive parts of the superhero description.
@@ -26,17 +26,23 @@ console.log(hero3 + " uses " + power3 + " to protect " + city3 + ".");
 // Step 1: Define a function with an appropriate name. 
 // Step 2: Idenitfy the inputs and add them as parameters. 
 // Setp 3: Add the function logic to return the full statement. 
-
+function heroAction(hero, power, city){
+  return hero + " uses " + power + " to protect " + city + "."
+}
 
 
 // Step 4: Call the function below for Spider-Man, Magneto, & Wonder Woman
 
+let hero1 = heroAction("spider man", "webslinging", "queens")
+console.log(hero1)
 
+console.log(heroAction("magneto", "magnetic manipulation", "genosha"))
 // 2. Classwork
 
 // Task 1: Add more superheroes!
 // Create at least two more superheroes by calling your function for each one.
-
+console.log(heroAction("batman", "fighting styles", "gotham"))
+console.log(heroAction("robin", "fighting styles", "gotham"))
 
 
 // Task 2: 5 members of the Sinister Six have escaped, and each villain is causing chaos in a different part of the city.
@@ -44,42 +50,46 @@ console.log(hero3 + " uses " + power3 + " to protect " + city3 + ".");
 
 // Step 1: Read through the following code and identify which parts are repetitive 
 
-let villain1 = "Doctor Octopus";
-let location1 = "Times Square";
-let chaos1 = "destroying cars with his mechanical arms";
-console.log(villain1 + " has escaped from the Raft and is causing chaos in " + location1 + " by " + chaos1 + "!");
+// let villain1 = "Doctor Octopus";
+// let location1 = "Times Square";
+// let chaos1 = "destroying cars with his mechanical arms";
+// console.log(villain1 + " has escaped from the Raft and is causing chaos in " + location1 + " by " + chaos1 + "!");
+console.log(villainAction("Doctor Octopus", "destroying cars with his mechanical arms", "Time Square"))
+// let villain2 = "Electro";
+// let location2 = "Central Park";
+// let chaos2 = "overloading the city's power grid";
+// console.log(villain2 + " has escaped from the Raft and is causing chaos in " + location2 + " by " + chaos2 + "!");
+console.log(villainAction("Electro", "overloading the city's power grid", "Central Park"))
+// let villain3 = "Vulture";
+// let location3 = "Empire State Building";
+// let chaos3 = "swooping down and grabbing people off the streets";
+// console.log(villain3 + " has escaped from the Raft and is causing chaos in " + location3 + " by " + chaos3 + "!");
+console.log(villainAction("Vulture", "swooping down and grabbing people off the streets", "Empire State Building"))
+// let villain4 = "Sandman";
+// let location4 = "Brooklyn Bridge";
+// let chaos4 = "turning the bridge into sand";
+// console.log(villain4 + " has escaped from the Raft and is causing chaos in " + location4 + " by " + chaos4 + "!");
+console.log(villainAction("Sandman", "turning the bridge into sand", "Brooklyn Bridge"))
+// let villain5 = "Mysterio";
+// let location5 = "Broadway";
+// let chaos5 = "casting illusions to confuse and panic everyone";
+// console.log(villain5 + " has escaped from the Raft and is causing chaos in " + location5 + " by " + chaos5 + "!");
+console.log(villainAction("Mysterio", "casting illusions to confuse and panic everyone", "Broadway"))
 
-let villain2 = "Electro";
-let location2 = "Central Park";
-let chaos2 = "overloading the city's power grid";
-console.log(villain2 + " has escaped from the Raft and is causing chaos in " + location2 + " by " + chaos2 + "!");
-
-let villain3 = "Vulture";
-let location3 = "Empire State Building";
-let chaos3 = "swooping down and grabbing people off the streets";
-console.log(villain3 + " has escaped from the Raft and is causing chaos in " + location3 + " by " + chaos3 + "!");
-
-let villain4 = "Sandman";
-let location4 = "Brooklyn Bridge";
-let chaos4 = "turning the bridge into sand";
-console.log(villain4 + " has escaped from the Raft and is causing chaos in " + location4 + " by " + chaos4 + "!");
-
-let villain5 = "Mysterio";
-let location5 = "Broadway";
-let chaos5 = "casting illusions to confuse and panic everyone";
-console.log(villain5 + " has escaped from the Raft and is causing chaos in " + location5 + " by " + chaos5 + "!");
 
 // Step 2: Define a function with an appropriate name. 
-
 // Step 3: Idenitfy the inputs and add them as parameters.
- 
 // Setp 4: Add the function logic to return the full statement. 
-
+ function villainAction (villain, chaos, location){
+    return villain + " has escaped from the raft and is causing chaos in " + location + " by " + chaos + "!"
+ }
 // Step 5: Call the function below for Doctor Octopus, Electro, Vulture, Sandman, Mysterio, & Kraven the Hunter
+
+
 
 // Step 6: Kraven the Hunter has also escaped, he's releasing and hunting wild animals in the Bronx Zoo
 // Call the function below for Kraven the Hunter
-
+console.log(villainAction("Kraven The Hunter","he's releasing and hunting wild animals in the", "Bronx Zoo" ))
 
 
 
@@ -107,13 +117,18 @@ let batman = {
 };
 
 // Step 2: Define a function with an appropriate name. 
-
 // Step 3: Idenitfy the inputs and add them as parameters.
- 
 // Setp 4: Add the function's logic to return the a super hero object. 
 
+function Action(secretIdentity, city, affiliation, archNemesis, catchphrase){
+  return secretIdentity + " lives in " + city + " associates with "+ affiliation + " fights " + archNemesis + " and says "+ catchphrase}
+
 // Step 5: Call the function below for Black Panther and Batman
+console.log(Action("Bruce Wayne", "Gotham City", "Justice League", "Joker", "I am vengeance, I am the night, I am Batman"))
+console.log(Action("T'Challa", "Wakanda", "Avengers", "Killmonger", "Wakanda Forever"))
 
 // Step 6: Call the function for Iron Man
 // Otherwise known as Tony Stark an Avenger stationed in NYC
 // He's known to say "I am Iron Man", and his greatest beef is with The Mandarin
+
+console.log(Action("Iron Man", "NYC", "Avengers", "The Mandarin", "I am Iron Man"))
